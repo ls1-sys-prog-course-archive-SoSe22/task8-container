68ff82d9639e4556c6b412c4220b00fde31afcf9
Jörg Thalheim <joerg@thalheim.io>
2021-03-15 19:49:12 +0100

    add script to prepare template

---
+++ prepare-template.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$#" -ne 1 ]; then
+    echo "USAGE: $0 task-name" >&2
+fi
+
+taskname=$1
+
+sed -i -e "s!task-name!${taskname}!" Makefile Cargo.toml
+mv task-name.cpp "${taskname}.cpp"
+mv task-name.c "${taskname}.c"
+rm "$0"
+git add task-name.cpp "${taskname}.cpp" task-name.c "${taskname}.c" "$0" Cargo.toml Makefile

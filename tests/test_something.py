74756ded86b33808d60ad05ce4c9b0887c07ae71
Jörg Thalheim <joerg@thalheim.io>
2021-03-17 11:22:33 +0100

    extend example

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+
+from testsupport import run_project_executable, warn, info
+
 def main() -> None:
-    # Put some test code here
-    pass
+    # Replace with the executable you want to test
+    try:
+        info("Run someprojectbinary...")
+        run_project_executable("someprojectbinary")
+        info("OK")
+    except OSError as e:
+        warn(f"Failed to run command: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":

1d0c9e827a92a2cb18cee1b02d6243272743cfe7
Jörg Thalheim <joerg@thalheim.io>
2021-06-20 22:24:06 +0200

    containersupport: provide fallback nix-build

--- tests/containersupport.py
+++ tests/containersupport.py
@@ -6,15 +6,27 @@ from typing import List
 import subprocess
 from testsupport import run, warn, info, test_root, run_project_executable, fail
 
+NIX_BUILD = "nix-build"
+
 
 def ensure_nix() -> None:
+    global NIX_BUILD
     if os.getuid() == 0:
-        warn("The script was run as uid 0. This task should not require root due to the user namespaces. Drop to nobody user")
+        warn(
+            "The script was run as uid 0. This task should not require root due to the user namespaces. Drop to nobody user"
+        )
 
     try:
-        run(["nix-build", "--version"])
+        run([NIX_BUILD, "--version"])
     except OSError as e:
-        fail(f"Could not run `nix-build --version` ({e}). Did you install nix correctly?")
+        # fall back in case /etc/profile is not correctly sourced
+        try:
+            NIX_BUILD = "/nix/var/nix/profiles/default/bin/nix-build"
+            run([NIX_BUILD, "--version"])
+        except OSError:
+            fail(
+                f"Could not run `nix-build --version` ({e}). Did you install nix correctly?"
+            )
 
 
 def nix_build_shell(builddir: Path, cmd: List[str]) -> str:
@@ -45,12 +57,24 @@ class BuildDir:
         if self.path.exists():
             return
         info(f"(Cached) Build path {self.path}")
-        res = run(["nix-build", "--keep-failed", "--builders", "", str(test_root().joinpath("nix"))], check=False, stderr=subprocess.PIPE)
+        res = run(
+            [
+                "nix-build",
+                "--keep-failed",
+                "--builders",
+                "",
+                str(test_root().joinpath("nix")),
+            ],
+            check=False,
+            stderr=subprocess.PIPE,
+        )
 
         print(res.stderr)
         match = re.search(r"keeping build directory '([^']+)'", res.stderr)
         if not match:
-            fail("could not find build directory of failing nix build in the output of `nix-build`")
+            fail(
+                "could not find build directory of failing nix build in the output of `nix-build`"
+            )
         self.path = Path(match.group(1))
         os.symlink(self.path, symlink_path)
         info(f"Build path {self.path}")
